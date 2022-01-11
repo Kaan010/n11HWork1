3de8dc8de49dcc7aa1fe0515819fd890a768e4ee
Kaan010 <kaankalan@outlook.com.tr>
2022-01-11 12:25:04 +0300

    deneme2.1

--- src/main/java/com/kaankln/cherrypickexample/exampleClass1.java
+++ src/main/java/com/kaankln/cherrypickexample/exampleClass1.java
@@ -4,4 +4,7 @@ public class exampleClass1 {
     public static void main(String[] args) {
         System.out.println("naber");
     }
+    private static void print(){
+        System.out.println("naber");
+    }
 }
