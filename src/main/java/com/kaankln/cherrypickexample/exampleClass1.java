3a0f7a2034f65895c6d68eb48868a56782f34ebd
Kaan010 <kaankalan@outlook.com.tr>
2022-01-11 12:12:11 +0300

    second commit

--- src/main/java/com/kaankln/cherrypickexample/exampleClass1.java
+++ src/main/java/com/kaankln/cherrypickexample/exampleClass1.java
@@ -4,4 +4,7 @@ public class exampleClass1 {
     public static void main(String[] args) {
         System.out.println("naber");
     }
+    void sayHi(){
+        System.out.println("hi");
+    }
 }
